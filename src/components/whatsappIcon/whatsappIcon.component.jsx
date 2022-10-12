b41d156486ba8b6fa4b6becd99a5f08288a53be2
luclongin <luc.longin@gmail.com>
2022-10-11 20:25:56 -0400

    whatsapp link opens in new tab

--- src/components/whatsappIcon/whatsappIcon.component.jsx
+++ src/components/whatsappIcon/whatsappIcon.component.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import './whatsappIcon.styles.scss';
 import whatsappLogo from '../../images/icon-whatsapp.png';
-import { Link } from "gatsby";
 
 const WhatsappIcon = () => {
       return(
             <div fixed="bottom" className="whatsapp-icon-fixed">
-                  <Link to="https://wa.me/59172384269">
+                  <a href="https://wa.me/59172384269" target="_blank" rel="noreferrer">
                         <img src={whatsappLogo} alt="Whatsapp Icon" />
-                  </Link>
+                  </a>
             </div>
       );
 }
